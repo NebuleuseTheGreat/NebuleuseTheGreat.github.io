413a7e30447e3c75866df5c6dfe0869a408a227d
ƝĒβULΞUSΣ <109994049+NebuleuseTheGreat@users.noreply.github.com>
2023-07-24 17:20:23 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+function openWindow() {
+  document.getElementById("window").style.display = "block";
+}
+
+function closeWindow() {
+  document.getElementById("window").style.display = "none";
+}
+
+
+
+function sendMessage() {
+  const messageInput = document.getElementById('messageInput');
+  const messageText = messageInput.value.trim();
+
+}
+
+
